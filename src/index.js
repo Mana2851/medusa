a861f57292a941a0ec8c9ad98229a4b9c116e6b7
Mana2851 <43323394+Mana2851@users.noreply.github.com>
2019-02-26 10:44:39 -0600

    u

--- src/index.js
+++ src/index.js
@@ -5,51 +5,32 @@ import './index.css';
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-	
+    this.state = { teamItems: [], workoutItems: [] }; //One for each so that each array can keep tract of its own list of items instead of changing the same one for both
   }
 
-  render() {
+  render()
+  {
     return (
-	<div id = "contain">
-      <div id = "teamName">
-        <h3>Team Names</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new_teamName">
-            Adding a new team?
-          </label>
-          <input
-            id="new_teamName"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
+      <div id = "contain">
+        <div id = "teamName">
+          <h3>Team Names</h3>
+          <TodoList items = {this.state.teamItems} text = {"teamList"} />
+        </div>
+        <div id = "workouts">
+          <h3>Workout Names</h3>
+          <TodoList items = {this.state.workoutItems} text = {"workoutList"} />
+        </div>
       </div>
-	  <div id = "workouts">
-        <h3>Workouts</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new_workout">
-            Adding a new workout?
-          </label>
-          <input
-            id="new_workout"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
-      </div>
-	</div>
     );
+  };
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: props.items, text: props.id };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
@@ -70,17 +51,37 @@ class TodoApp extends React.Component {
       text: ''
     }));
   }
+
+  render () {
+    const items = this.state.items;
+    //I passed the form into here as well so I could refer to "this" properly, since it seems that it always refers to the Component it is inside of.
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <ul id = {this.state.text}>
+          {items.map(item => <TodoItem item={item} key={item.key} />)}
+        </ul>
+        <label htmlFor = "new_Name">Adding a new workout?</label>
+        <input id = "new_Name" onChange = {this.handleChange} value = {this.state.text}/>
+        <button> Add #{this.state.items.length + 1}</button>
+      </form>
+    );
+  };
 }
 
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item, key: props.key }
+  }
+  render()
+  {
+    const item = this.state.item;
+    const key = this.state.key;
+    return(
+      <ul id = {key}>
+        {item.text}
       </ul>
-    );
+    )
   }
 }
 
